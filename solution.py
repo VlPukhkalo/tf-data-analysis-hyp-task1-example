30d8042fdf25041fb0b26ea4462de9225448300e
VlPukhkalo <60924860+VlPukhkalo@users.noreply.github.com>
2023-04-13 23:44:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,28 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 307311223 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    p1 = x_success/x_cnt
+    p2 = y_success/y_cnt
+    p = (x_success + y_success)/ (x_cnt + y_cnt)
+    
+    z = (p2-p1)/((p*(1-p)*(1/x_success + 1/y_success))**0.5)
+    
+    print(z)
+    
+    alpha = 0.01
+    
+    p_value = norm.cdf(z)
+    
+    print(p_value)
+    
+    if p_value < alpha: return True
+    else: return False
